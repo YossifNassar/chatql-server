4c474e21396a2a2024dd8e96ffe6e23a4ff8119c
Yossef Nassar <n.yossif@gmail.com>
2019-07-20 20:25:55 +0300

    resolvers module added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import fs from 'fs';
 import express from 'express';
 import { createServer } from 'http';
-import resolvers from './resolvers'
+import { ResolversProvider } from './resolvers'
+import { PubSub } from 'apollo-server';
+import { UsersDAO } from './data/users'
+import { ChannelsDAO } from './data/channels'
 
 const app = express();
 const PORT = 8080
 const typeDefs = gql(fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8'))
+const pubsub = new PubSub();
+const usersDAO = new UsersDAO(pubsub)
+const channelsDAO = new ChannelsDAO(pubsub, usersDAO)
+const resolversProvider = new ResolversProvider(pubsub,usersDAO, channelsDAO)
+const resolvers = resolversProvider.provideResolvers()
+
 const server = new ApolloServer({ typeDefs, resolvers, });
 
 server.applyMiddleware({ app, path: '/graphql' });
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,47 +1,51 @@
-import { PubSub, withFilter } from 'apollo-server';
-import { UsersDAO } from './data/users'
-import { ChannelsDAO } from './data/channels'
+import { withFilter } from 'apollo-server';
 
-const pubsub = new PubSub();
-const usersDAO = new UsersDAO(pubsub)
-const channelsDAO = new ChannelsDAO(pubsub, usersDAO)
+class ResolversProvider {
+    constructor(pubsub, usersDAO, channelsDAO) {
+        this.pubsub = pubsub;
+        this.usersDAO = usersDAO
+        this.channelsDAO = channelsDAO
+    }
 
-const resolvers = {
-    Query: {
-        user: (parent, args, context, info) => {
-            return usersDAO.getUserById(args.id)
-        },
-    },
-    Mutation: {
-        createUser: (parent, args, context, info) => {
-            return usersDAO.createUser(args.input)
-        },
-        createMessage: (parent, args, context, info) => {
-            const input = args.input
-            return channelsDAO.createMessage(input)
-        }
-    },
-    Subscription: {
-        onCreateUser: {
-            subscribe: () => pubsub.asyncIterator('createUser')
-        },
-        onCreateMessage: {
-            subscribe: withFilter(
-                () => pubsub.asyncIterator('createMessage'),
-                (payload, variables) => {
-                    return payload.channelId === variables.channelId;
+    provideResolvers() {
+        return {
+            Query: {
+                user: (parent, args, context, info) => {
+                    return this.usersDAO.getUserById(args.id)
+                },
+            },
+            Mutation: {
+                createUser: (parent, args, context, info) => {
+                    return this.usersDAO.createUser(args.input)
+                },
+                createMessage: (parent, args, context, info) => {
+                    const input = args.input
+                    return this.channelsDAO.createMessage(input)
                 }
-            )
-        }
-    },
-    User: {
-        channels: (user) => {
-            return channels.filter(channel => {
-                const ch = channel.users.find(u => u.id === user.id)
-                return ch != null
-            })
+            },
+            Subscription: {
+                onCreateUser: {
+                    subscribe: () => this.pubsub.asyncIterator('createUser')
+                },
+                onCreateMessage: {
+                    subscribe: withFilter(
+                        () => this.pubsub.asyncIterator('createMessage'),
+                        (payload, variables) => {
+                            return payload.channelId === variables.channelId;
+                        }
+                    )
+                }
+            },
+            User: {
+                channels: (user) => {
+                    return channels.filter(channel => {
+                        const ch = channel.users.find(u => u.id === user.id)
+                        return ch != null
+                    })
+                }
+            }
         }
     }
-};
+}
 
-export default resolvers
+export { ResolversProvider }
